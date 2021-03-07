65cf9409aaa624712b6a38d975e5139f9a438757
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-03-06 19:22:38 -0800

    Added basic image upload

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,12 +6,18 @@ import { Hello } from "./Hello";
 
 export const App = () => {
     const tasks = useTracker(() => TasksCollection.find({}).fetch());
-
+    const [image, setImage] = useState(undefined);
+    const onFileChange = (event) => {
+        console.log("Event", event.target.files);
+        setImage(event.target.files[0]);
+    };
+    console.log(image);
     return (
         <div>
             <Hello />
             <h1>Welcome to Meteor!</h1>
-
+            <input type="file" onChange={onFileChange} />
+            <img alt="Awaiting Image upload" src={image && URL.createObjectURL(image)} style={{ width: 200, aspectRatio: 1 }} />
             <ul>
                 { tasks.map((task) => <Task key={task._id} task={task} />) }
             </ul>
