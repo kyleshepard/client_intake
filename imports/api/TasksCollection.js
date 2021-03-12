f768091192915fad028c8e9d8d12d3721e9f9d42
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-03-12 08:34:51 -0800

    Implemented automatic Images cleanup

--- imports/api/TasksCollection.js
+++ imports/api/TasksCollection.js
@@ -1,3 +1,13 @@
 import { Mongo } from 'meteor/mongo';
+import { ImagesCollection } from "./ImagesCollection";
 
 export const TasksCollection = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+    // This listens for events in the collection
+    TasksCollection.find({}).observe({
+        removed: (oldDoc) => { // When an item was removed, delete the associated pictures
+            ImagesCollection.remove({ meta: { clientid: oldDoc._id } });
+        },
+    });
+}
