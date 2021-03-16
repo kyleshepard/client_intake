1fda3cfd4652553506e4cc824844cec6bc562ed4
Daniel Tucker <dtucker21@my.whitworth.edu>
2021-03-16 15:01:02 -0700

    Added material ui elements to Client.jsx

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { ImageDisplay } from "./ImageDisplay";
+import { Li } from '@material-ui/core';
+import { Input } from '@material-ui/core';
+import { Span } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 export const Client = ({ task, onCheckBoxClick, onDeleteClick }) => {
     console.log(task._id);
     return (
-        <li>
-            <input type="checkbox" checked={!!task.isChecked} onClick={() => onCheckBoxClick(task)} readOnly />
-            <span>{task.text}</span>
+        <Li>
+            <Input type="checkbox" checked={!!task.isChecked} onClick={() => onCheckBoxClick(task)} readOnly />
+            <Span>{task.text}</Span>
             <ImageDisplay clientid={task && task._id} />
-            <button onClick={() => onDeleteClick(task)}>Delete Client</button>
-        </li>
+            <Button onClick={() => onDeleteClick(task)}>Delete Client</Button>
+        </Li>
     );
 };
