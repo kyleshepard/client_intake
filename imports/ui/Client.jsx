a7612d281952afbaa3a09356f8704d2ec25abe8f
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-03-19 00:15:20 -0700

    Updating UI

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import {
-    Input, Button, Checkbox, ListItem, Divider,
+    Input, Button, Checkbox, ListItem, Divider, Paper, Grid,
 } from '@material-ui/core';
-
+import { theme } from "../api/theme";
 import { ImageDisplay } from "./ImageDisplay";
 
 export const Client = ({ task, onCheckBoxClick, onDeleteClick }) => (
-    <>
-        <ListItem>
-            <Checkbox checked={task.isChecked} onClick={() => onCheckBoxClick(task)} />
-            <span>{task.text}</span>
-            <ImageDisplay clientid={task && task._id} />
-            <Button onClick={() => onDeleteClick(task)}>Delete Client</Button>
-        </ListItem>
 
-        <Divider />
-    </>
+    <ListItem>
+        <Paper
+            style={{
+                padding: 5,
+                width:'100%',
+            }}
+            elevation={3}
+        >
+            <Grid container spacing={2} direction="row"
+                  justify="center"
+                  alignItems="center"
+            >
+                <Grid item sm={4}>
+                    {task.text}
+                </Grid>
+                <Grid item sm={4}>
+                    <ImageDisplay clientid={task && task._id} />
+                </Grid>
+                <Grid item sm={4}>
+                    <Button onClick={() => onDeleteClick(task)}>Delete Client</Button>
+                </Grid>
+            </Grid>
+        </Paper>
+    </ListItem>
+
 );
--- imports/ui/MainPage.jsx
+++ imports/ui/MainPage.jsx
@@ -1,10 +1,123 @@
 import React, { useState } from "react";
 import { useTracker } from "meteor/react-meteor-data";
+import { Button, List } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Badge from "@material-ui/core/Badge";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Drawer from "@material-ui/core/Drawer";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import { mainListItems, secondaryListItems } from "./dashboard_example/listItems";
 import { ClientForm } from "./ClientForm";
 import { Client } from "./Client";
 import { documentFields } from "../api/formConstants";
 import { ClientsCollection } from "../api/ClientsCollection";
-import {List} from "@material-ui/core";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://worldreliefspokane.org/">
+                World Relief Spokane
+            </Link>
+            {' '}
+            {new Date().getFullYear()}
+            .
+        </Typography>
+    );
+}
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    menuButtonHidden: {
+        display: 'none',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+        },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
 
 const toggleChecked = ({ _id, isChecked }) => {
     ClientsCollection.update(_id, {
@@ -15,6 +128,8 @@ const toggleChecked = ({ _id, isChecked }) => {
 };
 
 export const MainPage = () => {
+    /// //
+    // This part is for  logic
     console.log("FIELDS", documentFields);
     const deleteClient = ({ _id }) => ClientsCollection.remove(_id);
     const hideCompletedFilter = { isChecked: { $ne: true } };
@@ -24,23 +139,92 @@ export const MainPage = () => {
     const pendingClientsTitle = `${
         pendingClientsCount ? ` (${pendingClientsCount})` : ''
     }`;
-    return (
-        <>
-            <button onClick={Meteor.logout}>Log out</button>
-            <h1>
-                Clients
-                {pendingClientsTitle}
-            </h1>
 
-            <ClientForm />
-            <div className="filter">
-                <button onClick={() => setHideCompleted(!hideCompleted)}>
-                    {hideCompleted ? 'Show All' : 'Hide Completed'}
-                </button>
-            </div>
-            <List>
-                { tasks.map((task) => <Client key={task._id} task={task} onCheckBoxClick={toggleChecked} onDeleteClick={deleteClient} />)}
-            </List>
-        </>
+    /// ////////////This part is for display
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        Dashboard
+                    </Typography>
+                    <IconButton color="inherit">
+                        <Badge badgeContent={4} color="secondary">
+                            <NotificationsIcon />
+                        </Badge>
+                    </IconButton>
+                    <Button onClick={Meteor.logout} variant="contained">Log out</Button>
+
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>{mainListItems}</List>
+                <Divider />
+                <List>{secondaryListItems}</List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Grid container spacing={3}>
+
+                        {/* Add New Clients */}
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <ClientForm />
+                            </Paper>
+                        </Grid>
+                        {/* Display Clients */}
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <h1>
+                                    Potential Clients
+                                </h1>
+                                <List style={{
+                                  width: '100%',
+                                }}
+                                >
+                                    { tasks.map((task) => <Client key={task._id} task={task} onCheckBoxClick={toggleChecked} onDeleteClick={deleteClient} />)}
+                                </List>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    <Box pt={4}>
+                        <Copyright />
+                    </Box>
+                </Container>
+
+            </main>
+        </div>
+
     );
 };
