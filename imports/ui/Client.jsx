07ac1564a96b95c20e9462d38d6293436ccfdb18
Kyle Shepard <kshepard379@gmail.com>
2021-03-18 17:12:45 -0700

    got form template and user data somewhat implemented

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ImageDisplay } from "./ImageDisplay";
 
 export const Client = ({ task, onCheckBoxClick, onDeleteClick }) => {
-    console.log(task._id);
+    // console.log(task._id);
     return (
         <li>
             <input type="checkbox" checked={!!task.isChecked} onClick={() => onCheckBoxClick(task)} readOnly />
-            <span>{task.text}</span>
+            <span>{task.text}</span> 
             <ImageDisplay clientid={task && task._id} />
             <button onClick={() => onDeleteClick(task)}>Delete Client</button>
         </li>
--- imports/ui/ClientForm.jsx
+++ imports/ui/ClientForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ClientsCollection } from '../api/ClientsCollection';
+import { FormsCollection } from '../api/FormsCollection';
 import { documentFields } from "../api/formConstants";
 
 export const ClientForm = () => {
@@ -10,56 +11,17 @@ export const ClientForm = () => {
 
         if (!text) return;
 
-        // ClientsCollection.insert({
-        //     text: text.trim(),
-        //     createdAt: new Date(),
-        // });
-
-        // Object.keys(documentFields).forEach(key => {
-        //     console.log(documentFields[key]._id);
-        // });
-
         let client = {
             text: text.trim(),
-            formData: {},
+            data: {},
             createdAt: Date()
         };
-        
-        function recursive(field){
-            //intialize object
-            formData = {
-                field_name: field.name,
-                data: field.type
-            }
-            // console.log(field);
-            //if there are sub-fields with data that we need
-            if(field.childFields){
-                // console.log('in child fields');
-                //initialize childFields field
-                childFormData = {};
-                console.log(field.childFields);
-                //add each one to our set of child fields
-                field.childFields.forEach(childField => {
-                    // console.log(childField.name + " " + childField._id);
-                    // console.log(`--${childField.name} is a child of ${field.name} and has ${(childField.childFields != void(0)) ? childField.childFields.length : 0} children itself`);
-                    
-                //    childFormData[childField._id] = recursive(childField);
-                   console.log(childFormData[childField._id], field);
-                });
-                formData['childFormData'] = childFormData;
-            }
-            return formData;
-        }
-        
-        //eventually this will be a mongo collection, for now its defined in formConstants.ts
-        documentFields.forEach(field => {
-            // client['formData'][field._id] = {
-            //     field_name:  field.name,
-            //     data: null
-            // };
-            
-            client['formData'][field._id] = recursive(field);
+
+        //initialize every piece of data as boolean. how do we take in tons of values programmatically?
+        FormsCollection.find({}).fetch().forEach(field => {
+            client['data'][field._id] = false;
         });
+
         ClientsCollection.insert(client);
 
         setText("");
