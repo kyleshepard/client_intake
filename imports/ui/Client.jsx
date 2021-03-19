f90c8605c338ef12ab7eea7aa02460a0510eeef0
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-03-18 22:55:10 -0700

    Fixed blue screen

--- imports/ui/Client.jsx
+++ imports/ui/Client.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { Input, Button } from '@material-ui/core';
+
 import { ImageDisplay } from "./ImageDisplay";
-import { Li } from '@material-ui/core';
-import { Input } from '@material-ui/core';
-import { Span } from '@material-ui/core';
-import { Button } from '@material-ui/core';
 
 export const Client = ({ task, onCheckBoxClick, onDeleteClick }) => {
     console.log(task._id);
     return (
-        <Li>
+        <li>
             <Input type="checkbox" checked={!!task.isChecked} onClick={() => onCheckBoxClick(task)} readOnly />
-            <Span>{task.text}</Span>
+            <span>{task.text}</span>
             <ImageDisplay clientid={task && task._id} />
             <Button onClick={() => onDeleteClick(task)}>Delete Client</Button>
-        </Li>
+        </li>
     );
 };
