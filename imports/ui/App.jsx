7863c4d65abdf932a7491036509a59112bbe1da5
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-03-25 14:56:44 -0700

    Initial routing complete

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -10,51 +10,20 @@ import {
 import { LoginForm } from './LoginForm.jsx';
 import { MainPage } from "./MainPage/MainPage.jsx";
 import { theme } from "../api/theme";
+import {Routing} from "./Routing";
 
 export const App = () => {
     const user = useTracker(() => Meteor.user());
     return (
         <div className="main">
+
             <MuiThemeProvider theme={theme}>
                 <CssBaseline />
                 { user ? (
-                    <MainPage />
+                    <Routing />
                 ) : (
                     <LoginForm />
                 )}
-                <Router>
-                    <div>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <Link to="/">Home</Link>
-                                </li>
-                                <li>
-                                    <Link to="/about">About</Link>
-                                </li>
-                                <li>
-                                    <Link to="/users">Users</Link>
-                                </li>
-                            </ul>
-                        </nav>
-
-                        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                        <Switch>
-                            <Route path="/about">
-                                About
-                            </Route>
-                            <Route path="/users">
-                                Users
-                            </Route>
-                            <Route path="/">
-                                Home
-
-                            </Route>
-                        </Switch>
-                    </div>
-                </Router>
-                );
             </MuiThemeProvider>
         </div>
     );
