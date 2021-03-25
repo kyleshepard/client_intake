f10ab4fb5de733da8f71266ed6b7260e166a1577
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-03-25 14:42:51 -0700

    Slight bit of routing is happening

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
 import { LoginForm } from './LoginForm.jsx';
 import { MainPage } from "./MainPage/MainPage.jsx";
 import { theme } from "../api/theme";
@@ -16,6 +22,39 @@ export const App = () => {
                 ) : (
                     <LoginForm />
                 )}
+                <Router>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/users">Users</Link>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                        <Switch>
+                            <Route path="/about">
+                                About
+                            </Route>
+                            <Route path="/users">
+                                Users
+                            </Route>
+                            <Route path="/">
+                                Home
+
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+                );
             </MuiThemeProvider>
         </div>
     );
---
+++ imports/ui/Routing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+
+export const Routing = () => {
+    const x = 1;
+    return <div />;
+};
