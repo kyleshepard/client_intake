1e90c40855721ec8c695b988045a33a3b46b05db
Kyle Shepard <kshepard379@gmail.com>
2021-03-30 00:14:23 -0700

    fixed broken path for collections and form constants

--- imports/ui/MainPage/ClientForm.jsx
+++ imports/ui/MainPage/ClientForm.jsx
@@ -4,10 +4,10 @@ import {
 } from '@material-ui/core';
 import { useTracker } from "meteor/react-meteor-data";
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import { ClientsCollection } from '../api/ClientsCollection';
+// import { ClientsCollection } from '../api/ClientsCollection';
 import { Meteor } from 'meteor/meteor';
-import { FormsCollection } from '../api/FormsCollection';
-import { fieldTypes } from "../api/formConstants";
+import { FormsCollection } from '../../api/FormsCollection';
+import { fieldTypes } from "../../api/formConstants";
 
 export const ClientForm = () => {
     const [primaryData, setPrimaryData] = useState({});
