61b1522c06199acb43485002ef2cad99003490f9
Aiden McIlraith <amcilraith21@my.whitworth.edu>
2021-04-22 15:40:17 -0700

    Finished first formik form

--- imports/ui/SignUp.jsx
+++ imports/ui/SignUp.jsx
@@ -42,60 +42,6 @@ export const SignUp = () => {
     const history = useHistory();
     const classes = useStyles();
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [hasSubmitted, setHasSubmitted] = useState(false);
-
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            username: '',
-            password: '',
-        },
-        validationSchema: Yup.object({
-            firstName: Yup.string()
-            // .max(15, 'Must be 15 characters or less')
-                .required('Required'),
-            lastName: Yup.string()
-                // .max(20, 'Must be 20 characters or less')
-                .required('Required'),
-            email: Yup.string()
-                .email('Invalid email address')
-                .required('Required'),
-            password: Yup.string()
-                .required('Required'),
-        }),
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-
-    const submit = (e) => {
-        e.preventDefault();
-        // console.log(userData);
-        // alert(password);
-        try {
-            Meteor.call('users.register',
-                firstName,
-                lastName,
-                username,
-                password);
-            setHasSubmitted(true);
-            // return <Redirect exact to="/home" />;
-        } catch (error) {
-            alert(error);
-        }
-        setFirstName("");
-        setLastName("");
-        setUsername("");
-        setPassword("");
-    };
-    if (hasSubmitted) {
-        history.push("/");
-    }
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
@@ -120,87 +66,103 @@ export const SignUp = () => {
                         lastName: Yup.string()
                         // .max(20, 'Must be 20 characters or less')
                             .required('Required'),
-                        email: Yup.string()
-                            .email('Invalid email address')
+                        username: Yup.string()
+                            // .email('Invalid email address')
                             .required('Required'),
                         password: Yup.string()
                             .required('Required'),
                     })}
-                    onSubmit={(values) => {
-                        alert(JSON.stringify(values, null, 2));
+                    onSubmit={({
+                        firstName, lastName, username, password, ...values
+                    }) => {
+                        alert(JSON.stringify({firstName, lastName, username, password, ...values}, null, 2));
+                        try {
+                            Meteor.call('users.register',
+                                firstName,
+                                lastName,
+                                username,
+                                password);
+                            // return <Redirect exact to="/home" />;
+                            history.push("/");
+                        } catch (error) {
+                            alert(error);
+                        }
                     }}
                 >
-                    <Form>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <Field
-                                    component={TextField}
-                                    autoComplete="fname"
-                                    name="firstName"
-                                    id="firstName"
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    label="First Name"
-                                    autoFocus
+                    {({ submitForm, isSubmitting }) => (
+                        <Form>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <Field
+                                        component={TextField}
+                                        autoComplete="fname"
+                                        name="firstName"
+                                        id="firstName"
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        label="First Name"
+                                        autoFocus
 
-                                />
-                            </Grid>
+                                    />
+                                </Grid>
 
-                            <Grid item xs={12} sm={6}>
-                                <Field
-                                    component={TextField}
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="lname"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Field
-                                    component={TextField}
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    label="Username"
-                                    name="email"
-                                    autoComplete="username"
+                                <Grid item xs={12} sm={6}>
+                                    <Field
+                                        component={TextField}
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="lname"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Field
+                                        component={TextField}
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        label="Username"
+                                        name="username"
+                                        autoComplete="username"
 
-                                />
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Field
+                                        component={TextField}
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <Field
-                                    component={TextField}
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                />
+                            {isSubmitting && <LinearProgress />}
+                            <br />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                disabled={isSubmitting}
+                                onClick={submitForm}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justify="flex-end">
+                                <Grid item>
+                                    <Link variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justify="flex-end">
-                            <Grid item>
-                                <Link variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Form>
+                        </Form>
+                    )}
                 </Formik>
             </div>
             <Box mt={5}>
@@ -208,59 +170,5 @@ export const SignUp = () => {
             </Box>
 
         </Container>
-    );
-
-    return (
-        <Formik
-            initialValues={{
-                email: '',
-                password: '',
-            }}
-            validate={(values) => {
-                const errors = {};
-                if (!values.email) {
-                    errors.email = 'Required';
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-                ) {
-                    errors.email = 'Invalid email address';
-                }
-                return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    setSubmitting(false);
-                    alert(JSON.stringify(values, null, 2));
-                }, 500);
-            }}
-        >
-            {({ submitForm, isSubmitting }) => (
-                <Form>
-                    <Field
-                        component={TextField}
-                        name="email"
-                        type="email"
-                        label="Email"
-                    />
-                    <br />
-                    <Field
-                        component={TextField}
-                        type="password"
-                        label="Password"
-                        name="password"
-                    />
-                    {isSubmitting && <LinearProgress />}
-                    <br />
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        disabled={isSubmitting}
-                        onClick={submitForm}
-                    >
-                        Submit
-                    </Button>
-                </Form>
-            )}
-        </Formik>
     );
 };
